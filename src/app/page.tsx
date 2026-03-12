245df54e0c1aa6cf4c73415ce4e1203d55afcba8
Rychna Lukas <rychna@gmail.com>
2026-03-12 23:51:48 +0100

    change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <h1 className="text-5xl font-bold tracking-tight text-white sm:text-[6rem] hover:text-[6.5rem] transition-all duration-200">
-        <Link href="/planets">Go to planets</Link>
+        <Link href="/planets">Objev planety</Link>
       </h1>
     </div>
   );
