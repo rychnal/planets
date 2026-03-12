564318e26d9f43144217c7ac2d4294e84917150a
Rychna Lukas <rychna@gmail.com>
2026-03-12 23:03:35 +0100

    modify loading for server

--- src/app/planets/loading.tsx
+++ src/app/planets/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <p>Makáme na tom...</p>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-8 h-8 border-4 border-white/30 border-t-white rounded-full animate-spin" />
+    </div>
+  );
 }
